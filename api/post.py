7a34606848548c5210ffa77e0e2c55297e4c0c85
UbuntDude <wlf402@francis.edu>
2026-02-07 14:21:35 -0500

    Wrote post command file

--- api/post.py
+++ api/post.py
@@ -1,10 +1,10 @@
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
+import requests
 
 url= 'http://127.0.0.1:3000/'
-post_fields={'u':'me','p':'password'}
+final_url='http://httpbin.org/post'
+payload={'u':'username','p':'password'}
 
-request=Request(url, urlencode(post_fields).encode())
-with urlopen(request) as response:
-    json=response.read().decode()
-print(json)
+r=requests.post(final_url,data=payload)
+print(r.text)
--- api/server.py
+++ api/server.py
@@ -1,5 +1,10 @@
 import socket
 import select
+from http.server import BaseHTTPRequestHandler,HTTPServer
+
+class APIHandler(BaseHTTPRequestHandler):
+    u="username"
+    p="password"
 
 if __name__=="__main__":
     listener_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,24 +22,6 @@ if __name__=="__main__":
                 client_socket, client_address=ready_socket.accept()
                 client_msg=client_socket.recv(4096)
                 print(f"Client message: {client_msg.decode('utf-8')}")
-
-                client_socket.sendall(
-                    bytes(f"""H
-                    TTP/1.1 200 OK\r\nContent-type: text/html\r\nSet-Cookie: ServerName=wyattserver\r\r\n
-                    <!doctype html>
-                    <html>
-                        <head/>
-                        <body>
-                            <h1>Server address: {address_port[0]}:{address_port[1]}</h1>
-                            <h2>Connected through address: {client_address[0]}:{client_address[1]}</h2>
-                            <body>
-                                <pre>{client_msg.decode("utf-8")}<pre>
-                            <body>
-                        <body>
-                    <html>
-                    \r\n\r\n
-                    """, "utf-8")
-                )
                 try:
                     client_socket.close()
                 except OSError:
